6623fc672ae8bdcb2de725b9aaa6c51b854c0eef
ANDRESPC\andre <varaserrano.andres@gmail.com>
2023-07-31 23:45:48 +0200

    Binding dependency

--- app/src/main/java/vara17/loginfirebaseapp/activities/SignUpActivity.kt
+++ app/src/main/java/vara17/loginfirebaseapp/activities/SignUpActivity.kt
@@ -1,12 +1,63 @@
 package vara17.loginfirebaseapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import vara17.loginfirebaseapp.R
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import vara17.loginfirebaseapp.databinding.ActivitySignUpBinding
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setUpViews()
+
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            Toast.makeText(this, "User is NOT logged in", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "User is logged in", Toast.LENGTH_LONG).show()
+        }
+
+
+        createAccount("varaserrano.andres@gmail.com", "123456")
+    }
+
+    private fun setUpViews() {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth?.let {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(
+                            this,
+                            "createUserWithEmailAndPassword: succes",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        val user = auth.currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            this,
+                            "createUserWithEmailAndPassword: fail",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
     }
 }
